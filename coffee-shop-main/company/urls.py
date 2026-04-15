db840892095d861dba7c8b24e1bd5e4f5a0e2ba2
Tran Quang Thinh <tranquangthinh2006s@gmail.com>
2026-04-15 15:43:41 +0700

    Fix template inheritance order and load crispy tags

--- coffee-shop-main/company/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.contact, name='contact'),
-    path('base.html', views.newsletter, name='newsletter'),
-    path('about/', views.about.as_view(), name='about'),
-]
